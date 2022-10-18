3cea1ac5f63eb8e665fc69671a23c9320e3b2e65
Kim JeongUng <82865325+Kim-JeongUng@users.noreply.github.com>
2022-10-18 18:23:18 +0900

    Initial commit

---
+++ Assets/SampleScript.ts
@@ -0,0 +1,13 @@
+import { ZepetoScriptBehaviour } from 'ZEPETO.Script'
+
+export default class SampleScript extends ZepetoScriptBehaviour {
+
+    Start() {    
+        console.log("Hello ZEPETO Scriptaaaa");
+    }
+
+    Update() {
+        this.transform.Rotate(3, 0, 0);
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# ZEPETO-Practice
+ 제페토 월드제작 연습 
